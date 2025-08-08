271bb9ba7132a6b23b820b398642d64469b17f17
Tiger Abrodi <tigerabrodi@gmail.com>
2025-08-08 07:49:52 +0200

    routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 
 import { Toaster } from './components/ui/sonner'
 import { AuthenticatedLayout } from './layouts/authenticated'
 import { ROUTES } from './lib/constants'
-import { HomePage } from './pages/home'
+import { CharacterCreate } from './pages/character-create'
+import { CharacterDetail } from './pages/character-detail'
+import { CharacterEdit } from './pages/character-edit'
+import { ImageDetail } from './pages/character-image-detail'
+import { VideoDetail } from './pages/character-video-detail'
+import { CharactersGrid } from './pages/characters'
 import { LoginPage } from './pages/login'
 
 export function App() {
@@ -12,7 +17,13 @@ export function App() {
       <Routes>
         <Route path={ROUTES.login} element={<LoginPage />} />
         <Route element={<AuthenticatedLayout />}>
-          <Route path={ROUTES.home} element={<HomePage />} />
+          <Route path="/" element={<Navigate to={ROUTES.characters} replace />} />
+          <Route path={ROUTES.characters} element={<CharactersGrid />} />
+          <Route path={ROUTES.characterEdit} element={<CharacterEdit />} />
+          <Route path={ROUTES.characterCreate} element={<CharacterCreate />} />
+          <Route path={ROUTES.characterDetail} element={<CharacterDetail />} />
+          <Route path={ROUTES.characterImageDetail} element={<ImageDetail />} />
+          <Route path={ROUTES.characterVideoDetail} element={<VideoDetail />} />
         </Route>
       </Routes>
       <Toaster />
